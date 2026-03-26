fb912f689361ab1dc4938cecc530d33e8d1dac00
rajvardhan1110 <rajvardhanpatil1110@gmail.com>
2026-03-26 12:08:03 +0530

    rust basics

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    // println!("Hello, world!");
+
+
+    //variables
+    //numbers
+    let x : i32 = -5;
+    let y : u32 = 1000;
+    let z : f32 = 10.05;
+
+
+    println!("x : {}",x);
+    println!("y : {}",y);
+    println!("z : {}",z);
+
+    //boolean
+    let is_male:bool = true;
+
+    if is_male{
+        println!("you are a male");
+    }else{
+        println!("you are not a male");
+    }
+
+    //string 
+    let greeting = String::from("hello world");
+    println!("{}", greeting);
+
+    //conditional 
+
+    let number = 10;
+
+    if number > 5 {
+        println!("Number is greater than 5");
+    } else {
+        println!("Number is 5 or less");
+    }
+
+
+    //loop
+
+    for i in 1..5 {
+        print!("{} ", i);
+    }
+    println!();
+
+    let mut i = 1;
+
+    while i <= 3 {
+        print!("{} ", i);
+        i += 1;
+    }
+    println!();
+
+
+    //functions
+
+    let result = add(5, 3);
+    println!("5 + 3 = {}", result);
+
+    //struct & impl
+
+    let p = Person { name: String::from("Raj") };
+    p.greet();
+
+    
+
+}
+
+struct Person {
+    name: String,
+}
+
+impl Person {
+    fn greet(&self) {
+        println!("Hello {}", self.name);
+    }
+}
+
+fn add(a: i32, b: i32) -> i32 {
+   return a + b  
+}
